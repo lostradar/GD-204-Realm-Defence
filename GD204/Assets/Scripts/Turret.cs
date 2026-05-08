d2ef0b1ee41d1ecbb2270de1cacd96f0b43162ab
shadow3989 <louiestaples29@gmail.com>
2026-05-08 15:26:33 +0100

    death particle fix
    death particle fix (and added the animation sprites)

--- GD204/Assets/Scripts/Turret.cs
+++ GD204/Assets/Scripts/Turret.cs
@@ -96,7 +96,7 @@ public class Turret : MonoBehaviour
     {
         Vector2 direction = target.transform.position - transform.position;
 
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(direction.y, direction.x + 90) * Mathf.Rad2Deg;
 
         transform.rotation = Quaternion.Euler(0, 0, angle);
     }
